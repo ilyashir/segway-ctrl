0c3fcfce9b96cc464528dfc1697a5170a4bb4589
Ilya Shirokolobov <ilyash@trikset.com>
2013-12-20 23:06:39 +0400

    Concurrent threads

---
+++ stabilizer.cpp
@@ -0,0 +1,6 @@
+#include "stabilizer.h"
+
+Stabilizer::Stabilizer(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ stabilizer.h
@@ -0,0 +1,18 @@
+#ifndef STABILIZER_H
+#define STABILIZER_H
+
+#include <QObject>
+
+class Stabilizer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Stabilizer(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // STABILIZER_H
